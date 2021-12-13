f1b9bd20b83e906fe3e63222c7b101c95b9b3e54
Guillaume Berthier <guillaume.berthier@fabernovel.com>
2021-12-13 15:32:44 +0100

    Implement day12 part1

--- 2021/day12.py
+++ 2021/day12.py
@@ -16,26 +16,50 @@ for row in rows:
         graph[left].append(right)
     else:
         graph[left] = [right]
-
-
-def get_neighbors(node, graph, path):
-    if node == 'end':
-        return []
-    return [x for x in graph[node] if x not in path or x.isupper()]
+    if right in graph:
+        graph[right].append(left)
+    else:
+        graph[right] = [left]
 
 
 # Part 1
-solutions = []
-to_explore = deque(['start'])
-current_solution = []
-while to_explore:
-    current_node = to_explore.popleft()
-    current_solution.append(current_node)
-    if current_node is 'end':
-        solutions.append(current_solution)
-        continue
-    neighbors = get_neighbors(current_node, graph, current_solution)
-    if not neighbors:
-        current_solution.pop()
-        continue
+def get_neighbors(node, graph, visited_nodes):
+    return [x for x in graph[node] if x not in visited_nodes or x.isupper()]
 
+
+# Part 2
+def get_neighbors_2(node, graph, visited_nodes):
+    forbidden_nodes = set()
+    for visited_node in visited_nodes:
+        if visited_nodes.count(visited_node) > 2:
+            forbidden_nodes.add(visited_node)
+    forbidden_nodes.add('start')
+    twice_nodes = set(filter(lambda x: visited_nodes.count(x) == 2, visited_nodes))
+    twice_nodes_scenarios = [twice_nodes.difference(x) for x in twice_nodes]
+    if twice_nodes_scenarios:
+        forbidden = [forbidden_nodes.union(x) for x in twice_nodes_scenarios]
+        result = []
+        for forb in forbidden:
+            result.append([x for x in graph[node] if x not in forb or x.isupper()])
+        return result
+    else:
+        forbidden = forbidden_nodes.copy()
+        return [[x for x in graph[node] if x not in forbidden or x.isupper()]]
+
+
+part1_solutions = set()
+
+
+def get_all_paths(source, destination, graph, visited_nodes, path):
+    visited_nodes.append(source)
+    path.append(source)
+    if source == destination:
+        part1_solutions.add(','.join(path))
+        return
+    neighbors = get_neighbors(source, graph, visited_nodes)
+    for neighbor in neighbors:
+        get_all_paths(neighbor, destination, graph, visited_nodes.copy(), path.copy())
+
+
+get_all_paths('start', 'end', graph, [], [])
+print(len(part1_solutions))
